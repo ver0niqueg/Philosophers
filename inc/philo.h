24a5e076b1906b2720771cc5083495e138932f8a
Veronique Galmiche <vgalmich@bess-f3r4s2.clusters.42paris.fr>
2024-11-26 20:29:02 +0100

    added

--- inc/philo.h
+++ inc/philo.h
@@ -16,12 +16,15 @@ typedef struct	s_philo
 	int					is_eating; // si le philo est en train de manger
 	int					meals_eaten; // le nb de repas que le philo a mange -> progression du philo
 	int					nb_times_to_eat; // objectif a atteindre (cb il doit manger)
-	int					*dead;
+	int					*dead; //
 	size_t				time_to_die;
 	size_t				time_to_eat;
 	size_t				time_to_sleep;
 	size_t				last_meal; // le temps du dernier repas du philo
 	size_t				start_time; // le temps auquel le philo commence son activite
-	pthread_mutex_t		right_fork;
-	pthread_mutex_t		left_fork;
-}				t_philo;
+	pthread_mutex_t		*right_fork;
+	pthread_mutex_t		*left_fork;
+	pthread_mutex_t		*write_lock; //
+	pthread_mutex_t		*dead_lock;
+	pthread_mutex_t		*meal_lock;
+}						t_philo;
